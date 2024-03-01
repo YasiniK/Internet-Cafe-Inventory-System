3af21a52f40fd99c11ae47fc0737983f6d50b748
YasiniK <yasinikarunanayake@gmail.com>
2024-03-01 15:31:11 +0530

    Changes

--- InventorySystem/src/main/java/module-info.java
+++ InventorySystem/src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.example.inventorysystem {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires org.kordamp.ikonli.core;
+    requires org.kordamp.ikonli.fontawesome5;
+    requires org.kordamp.ikonli.javafx;
+
+
     opens com.example.inventorysystem to javafx.fxml;
     exports com.example.inventorysystem;
 }
